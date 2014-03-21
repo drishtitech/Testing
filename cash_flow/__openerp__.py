4e40b46a2627ebf4de36c7ad9fc9ae9ce398de59
Kaushik Kudtarkar <kaushik.kudtarkar@drishtigroup.com>
2014-03-21 16:56:49 +0530

    Kaushik code

---
+++ cash_flow/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Cash Flow Analysis',
+    'version': '1.0',
+    'category': 'Tools',
+    'sequence': 14,
+    'summary': 'Cash Flow Analysis',
+    'description': """
+Cash Flow Analysis
+    """,
+    'author': 'Drishti Tech',
+    'website': 'http://www.drishtitech.com',
+    'depends': ['account','mail'],
+    'data': ['cash_flow_data.xml','cash_flow.xml'],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
